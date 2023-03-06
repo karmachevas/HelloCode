2aff2e0a24a85423a405a3bd0820191a9541139c
karmachevas <karmachev.as@yandex.ru>
2023-03-06 23:17:32 +0500

    add Examples005_IfElse

---
+++ Examples005_IfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "admin") 
+{
+    Console.Write("Привет администратор");
+}
+else
+{
+    Console.Write("Привет, пользователь ");
+    Console.WriteLine(username);
+}
